abc1a89e5346205a983d76596142f6d2e88d921b
Christian Groothoff <ruffo324@gmail.com>
2018-09-12 21:40:28 +0200

    ToTheCore/Utils#1 Adding IEMP to fullinstaller and updater

--- fullInstaller.lua
+++ fullInstaller.lua
@@ -4,10 +4,12 @@ print("Downloading \"installer\" files..")
 term.setTextColor(colors.gray)
 -- Installer files download.
 shell.run("/openp/github", "get", "ToTheCore/Downloader/master/fullUpdater.lua","/fullUpdater")
+shell.run("/openp/github", "get", "ToTheCore/Downloader/master/wget.lua","/wget")
 
 shell.run("/openp/github", "get", "ToTheCore/Utils/master/installer.lua","/Utils/installer")
 shell.run("/openp/github", "get", "ToTheCore/Interface2Inventory/master/installer.lua","/Interface2Inventory/installer")
 shell.run("/openp/github", "get", "ToTheCore/MonitorHandler/master/installer.lua","/MonitorHandler/installer")
+shell.run("/openp/github", "get", "ToTheCore/IEMP/master/installer.lua","/IEMP/installer")
 
 -- Run installer files
 term.setTextColor(colors.green)
@@ -17,3 +19,4 @@ term.setTextColor(colors.gray)
 shell.run("/Utils/installer")
 shell.run("/Interface2Inventory/installer")
 shell.run("/MonitorHandler/installer")
+shell.run("/IEMP/installer")
--- fullUpdater.lua
+++ fullUpdater.lua
@@ -5,5 +5,6 @@ term.setTextColor(colors.gray)
 shell.run("/Utils/updater")
 shell.run("/Interface2Inventory/updater")
 shell.run("/MonitorHandler/updater")
+shell.run("/IEMP/updater")
 
 -- TODO: update /Downloader/*
